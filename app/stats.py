a0a0e0848bacea82fb5153e10dc28c6b25d877b7
eliadca <camachoeliad@gmail.com>
2024-12-06 15:32:43 -0500

    Add route for tables.html in stats.py

--- app/stats.py
+++ app/stats.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+from app.database import get_db_connection
 
 stats_bp = Blueprint('stats', __name__, url_prefix='/stats')
 
 @stats_bp.route('/')
 def stats_home():
     return render_template('stats.html')
+
+@stats_bp.route('/tables')
+def stats_tables():
+    return render_template('tables.html')
