dce57458c5469766d39a39c26fd94bb59264fade
eliadca <camachoeliad@gmail.com>
2024-12-06 16:18:29 -0500

    Set explicit template folder and update host/port

--- app/home.py
+++ app/home.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from .stats import stats_bp
 from .tables import tables_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 app.register_blueprint(stats_bp)
 app.register_blueprint(tables_bp)
 
@@ -11,4 +11,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5005)
